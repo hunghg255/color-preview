cd5f9f1953082cb34c8177fc95e83fe6f6b5f03c
hunghg255 <giahunghust@gmail.com>
2024-01-24 11:03:12 +0700

    chore: update svg

--- src/utils/svg.ts
+++ src/utils/svg.ts
@@ -6,5 +6,5 @@ export function toDataUrl(str: string) {
 }
 
 export function getSVG(c: string) {
-  return `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="${c || color.value}" d="M12 22q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22"/></svg>`
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="${c || color.value}" d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10s10-4.47 10-10S17.53 2 12 2"/></svg>`
 }
---
+++ test/fixture/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Test() {
+  return (
+    <>
+      <p className="primary"></p>
+    </>
+  )
+}
+
+export default Test
